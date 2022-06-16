5133c0237dc701e52f5a98f1bec3cb0f5efc222a
lauren <Minji@lauren.local>
2022-06-16 20:50:48 +1200

    commit before pull

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <div className="title">
-        <img src="/images/color_earth.gif" alt="A coloured globe of the earth spinning around on its axis"/>
         <h1>Drum Machine Woo</h1>
       </div>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
