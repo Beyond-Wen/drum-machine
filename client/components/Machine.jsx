6bbdafe668dd4822f6aa63bb0f3c8fb15914bbbe
jojo-mepham <jomepham@gmail.com>
2022-06-16 19:09:02 +1200

    ready to merge

--- client/components/Machine.jsx
+++ client/components/Machine.jsx
@@ -5,8 +5,12 @@ function Machine() {
     <div>
       <h2>Home</h2>
       <p></p>
+      <audio controls>
+        <source src='./sounds/drum-10' type='wav'></source>
+      </audio>
     </div>
   )
 }
 
+{/* <Pixel img={} sound={} id{} */}
 export default Machine
