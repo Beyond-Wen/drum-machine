7061ed2960362aed7ce21b511572731ba296bf28
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-06-17 10:02:35 +1200

    randomsound fixed

--- client/components/Machine.jsx
+++ client/components/Machine.jsx
@@ -3,29 +3,32 @@ import DrumPixel from './DrumPixel'
 import drumData from '../../data/drums'
 
 function Machine() {
-  function randomDrum(e) {
-    e.preventDefault()
-    // length 12
-    // 0 - 0.99999 x 12 = 0- 11.999999999999 = 0 - 11
-    const randNum = Math.floor(Math.random() * drumData.length)
-    const audio = new Audio(`/sounds/drum-${randNum}.wav`)
-    audio.play()
-  }
-
   return (
     <div className="pixels">
       {drumData.map((pixel, i) => {
         const [color, setColor] = useState('white')
         const colors = ['blue', 'green', 'yellow', 'white', 'black']
+
         function changeColor(e) {
-          e.preventDefault()
           const randNum = Math.floor(Math.random() * colors.length)
           setColor(colors[randNum])
+        }
+
+        function randomDrum(e) {
+          e.preventDefault()
+          // length 12
+          // 0 - 0.99999 x 12 = 0- 11.999999999999 = 0 - 11
+          const randNum = Math.floor(Math.random() * drumData.length)
+          const audio = new Audio(`/sounds/drum-${randNum}.wav`)
+          audio.play()
+
+          changeColor()
         }
 
         function changeColorToWhite(e) {
           setColor('white')
         }
+
         return (
           <DrumPixel
             key={pixel.drumId}
@@ -34,7 +37,7 @@ function Machine() {
             image={pixel.image}
             index={i}
             color={color}
-            rightClickFunc={changeColor}
+            rightClickFunc={randomDrum}
             changeColorToWhite={changeColorToWhite}
           />
         )
